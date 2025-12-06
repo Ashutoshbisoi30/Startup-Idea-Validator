715d0618e56725bdfb04c1f3bc133b2928610ec2
Ashutoshbisoi30 <ashutoshbisoi01@gmail.com>
2025-12-06 17:16:23 +0530

    Fix CORS issue

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://startup-idea-validator-backend.onrender.com",
+    origin: ["https://startup-idea-validator-seven.vercel.app",
     "http://localhost:5173"],
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
